beac76950996cc85bbe20a72bb2a1156460b69f6
heyy-josval <josvalmtrz@gmail.com>
2024-08-16 12:13:00 -0500

    first commit

---
+++ src/main/java/com/josval/sales_management/model/entity/User.java
@@ -0,0 +1,35 @@
+package com.josval.sales_management.model.entity;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.io.Serializable;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@ToString
+@Builder
+@Entity
+@Table(name = "users")
+public class User implements Serializable {
+    @Id
+    @Column(name = "id")
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    @Column(name = "name")
+    private String name;
+
+    @Column(name = "lastname")
+    private String lastname;
+
+    @Column(name = "email")
+    private String email;
+
+    @Column(name = "password")
+    private String password;
+
+    @Column(name = "phone")
+    private String phone;
+}
