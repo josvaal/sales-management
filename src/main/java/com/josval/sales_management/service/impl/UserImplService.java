96ba9483d1fd2a4d7ebdfff746580c8c8ddb50cf
heyy-josval <josvalmtrz@gmail.com>
2024-08-17 13:24:37 -0500

    feat: Add User Implementation Service

---
+++ src/main/java/com/josval/sales_management/service/impl/UserImplService.java
@@ -0,0 +1,62 @@
+package com.josval.sales_management.service.impl;
+
+import com.josval.sales_management.model.dao.UserDAO;
+import com.josval.sales_management.model.dto.UserDTO;
+import com.josval.sales_management.model.entity.User;
+import com.josval.sales_management.service.IUserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+import java.util.Objects;
+
+public class UserImplService implements IUserService {
+    @Autowired
+    private UserDAO userDao;
+
+    @Transactional(readOnly = true)
+    @Override
+    public List<User> listAll() {
+        return (List<User>) userDao.findAll();
+    }
+
+    @Transactional
+    @Override
+    public User save(UserDTO userDto) {
+        User user = User.builder()
+                .id(userDto.getId())
+                .name(userDto.getName())
+                .lastname(userDto.getLastname())
+                .role(userDto.getRole())
+                .email(userDto.getEmail())
+                .password(userDto.getPassword())
+                .build();
+        return userDao.save(user);
+    }
+
+    @Transactional(readOnly = true)
+    @Override
+    public User findById(Integer id) {
+        return userDao.findById(id).orElse(null);
+    }
+
+    @Transactional
+    @Override
+    public void delete(User user) {
+        userDao.delete(user);
+    }
+
+    @Override
+    public boolean existsById(Integer id) {
+        return userDao.existsById(id);
+    }
+
+    @Override
+    public boolean isAdminById(Integer id) {
+        User user = userDao.findById(id).orElse(null);
+        if(user == null){
+            return false;
+        }
+        return Objects.equals(user.getRole(), "admin");
+    }
+}
