1174ec17ba644cab590662aac1c1fe8a1c8bb6f9
heyy-josval <josvalmtrz@gmail.com>
2024-08-17 14:03:15 -0500

    feat: Add method findAllByUserId

--- src/main/java/com/josval/sales_management/model/dao/SaleDAO.java
+++ src/main/java/com/josval/sales_management/model/dao/SaleDAO.java
@@ -3,5 +3,8 @@ package com.josval.sales_management.model.dao;
 import com.josval.sales_management.model.entity.Sale;
 import org.springframework.data.repository.CrudRepository;
 
+import java.util.List;
+
 public interface SaleDAO extends CrudRepository<Sale, Integer> {
+    List<Sale> findAllByUserId(Integer userId);
 }
