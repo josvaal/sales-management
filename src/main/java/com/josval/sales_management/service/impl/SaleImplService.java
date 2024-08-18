283036a8655b5a4e209d6301a3bebe9c1b3b1c0f
heyy-josval <josvalmtrz@gmail.com>
2024-08-18 12:20:06 -0500

    fix: The SaleDTO builder in create method
    I add the annotation @Service

--- src/main/java/com/josval/sales_management/service/impl/SaleImplService.java
+++ src/main/java/com/josval/sales_management/service/impl/SaleImplService.java
@@ -5,10 +5,12 @@ import com.josval.sales_management.model.dto.SaleDTO;
 import com.josval.sales_management.model.entity.Sale;
 import com.josval.sales_management.service.ISaleService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 
+@Service
 public class SaleImplService implements ISaleService {
     @Autowired
     private SaleDAO saleDao;
