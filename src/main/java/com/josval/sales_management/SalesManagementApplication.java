783da95e0597789698e16b4484a2e150bc37084d
heyy-josval <josvalmtrz@gmail.com>
2024-08-18 12:20:31 -0500

    feat: Add scanBasePackages in main

--- src/main/java/com/josval/sales_management/SalesManagementApplication.java
+++ src/main/java/com/josval/sales_management/SalesManagementApplication.java
@@ -3,7 +3,7 @@ package com.josval.sales_management;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
+@SpringBootApplication(scanBasePackages = "com.josval.sales_management")
 public class SalesManagementApplication {
 
 	public static void main(String[] args) {
