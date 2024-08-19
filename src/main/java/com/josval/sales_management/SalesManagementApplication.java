4bfea80d74292fea8b8699d1ed5226b326f05898
heyy-josval <josvalmtrz@gmail.com>
2024-08-18 22:03:20 -0500

    feat: Add the dotenv init

--- src/main/java/com/josval/sales_management/SalesManagementApplication.java
+++ src/main/java/com/josval/sales_management/SalesManagementApplication.java
@@ -1,5 +1,6 @@
 package com.josval.sales_management;
 
+import io.github.cdimascio.dotenv.Dotenv;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -7,6 +8,10 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class SalesManagementApplication {
 
 	public static void main(String[] args) {
+		Dotenv dotenv = Dotenv.load();
+
+		System.setProperty("encrypt.secret_key", dotenv.get("ENCRYPT_SECRET_KEY"));
+
 		SpringApplication.run(SalesManagementApplication.class, args);
 	}
 
