5eb1a4985fa39dfdb1768856bf3cac9f6958bb70
heyy-josval <josvalmtrz@gmail.com>
2024-08-18 21:34:17 -0500

    feat: Data Access Object of search by user email

--- src/main/java/com/josval/sales_management/model/dao/UserDAO.java
+++ src/main/java/com/josval/sales_management/model/dao/UserDAO.java
@@ -3,5 +3,8 @@ package com.josval.sales_management.model.dao;
 import com.josval.sales_management.model.entity.User;
 import org.springframework.data.repository.CrudRepository;
 
+import java.util.Optional;
+
 public interface UserDAO extends CrudRepository<User, Integer> {
+  Optional<User> findByEmail(String email);
 }
