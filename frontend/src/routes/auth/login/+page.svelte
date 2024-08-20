2156670dd702ef66944e5a0e603ec27e3d742ca3
heyy-josval <josvalmtrz@gmail.com>
2024-08-19 19:20:01 -0500

    feat: Login page design

---
+++ frontend/src/routes/auth/login/+page.svelte
@@ -0,0 +1,30 @@
+<div class="w-full h-screen flex justify-center items-center">
+	<section class="bg-gray-2 rounded-xl p-5">
+		<div class="w-full text-center mb-5">
+			<h1 class="text-2xl font-bold">User Login</h1>
+		</div>
+		<form action="" class="flex flex-col gap-3">
+			<div class="w-full">
+				<input
+					class="input input-solid max-w-full"
+					placeholder="Email"
+					type="text"
+					id="name"
+					required
+				/>
+			</div>
+			<div class="w-full">
+				<input
+					class="input input-solid max-w-full"
+					placeholder="Password"
+					type="password"
+					id="name"
+					required
+				/>
+			</div>
+			<div class="w-full">
+				<button class="btn btn-primary w-full text-black font-bold" type="submit">Login</button>
+			</div>
+		</form>
+	</section>
+</div>
