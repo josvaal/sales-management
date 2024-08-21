f091fdf03955c874487ca93081519d9bb678742d
heyy-josval <josvalmtrz@gmail.com>
2024-08-20 23:51:29 -0500

    feat:Modulate the sidebar header

---
+++ frontend/src/lib/modules/core/components/Sidebar/SidebarHeader.svelte
@@ -0,0 +1,11 @@
+<script>
+	import moneyIcon from '$lib/assets/money.svg?raw';
+	import { data } from '$lib/index.js';
+</script>
+
+<section class="sidebar-title items-center p-4">
+	{@html data.icon}
+	<div class="flex flex-col">
+		<span>{data.title}</span>
+	</div>
+</section>
