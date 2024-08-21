e054f8c3e5ef0b4d9fbfcff5e0a0460f5183ab98
heyy-josval <josvalmtrz@gmail.com>
2024-08-20 23:51:43 -0500

    feat: Modulate the sidebar footer

---
+++ frontend/src/lib/modules/core/components/Sidebar/SidebarFooter.svelte
@@ -0,0 +1,38 @@
+<section class="sidebar-footer bg-gray-2 pt-2">
+	<div class="divider my-0"></div>
+	<div class="dropdown z-50 flex h-fit w-full cursor-pointer hover:bg-gray-4">
+		<!-- svelte-ignore a11y-no-noninteractive-tabindex -->
+		<!-- svelte-ignore a11y-label-has-associated-control -->
+		<label class="whites mx-2 flex h-fit w-full cursor-pointer p-0 hover:bg-gray-4" tabindex="0">
+			<div class="flex flex-row gap-4 p-4">
+				<div class="avatar avatar-md">
+					<img
+						src="https://static.vecteezy.com/system/resources/previews/030/750/807/non_2x/user-icon-in-trendy-outline-style-isolated-on-white-background-user-silhouette-symbol-for-your-website-design-logo-app-ui-illustration-eps10-free-vector.jpg"
+						alt="avatar"
+					/>
+				</div>
+
+				<div class="flex flex-col">
+					<span>Admin</span>
+					<span class="text-xs font-normal text-content2">Admin</span>
+				</div>
+			</div>
+		</label>
+		<div class="dropdown-menu-right-top dropdown-menu ml-2">
+			<!-- svelte-ignore a11y-missing-attribute -->
+			<a class="dropdown-item text-sm menu-item-disabled">Profile</a>
+			<!-- svelte-ignore a11y-missing-attribute -->
+			<a tabindex="-1" class="dropdown-item text-sm menu-item-disabled">Account settings</a>
+			<!-- svelte-ignore a11y-missing-attribute -->
+			<a tabindex="-1" class="dropdown-item text-sm menu-item-disabled">Change email</a>
+			<!-- svelte-ignore a11y-missing-attribute -->
+			<a tabindex="-1" class="dropdown-item text-sm menu-item-disabled">Subscriptions</a>
+			<!-- svelte-ignore a11y-missing-attribute -->
+			<a tabindex="-1" class="dropdown-item text-sm menu-item-disabled">Change password</a>
+			<!-- svelte-ignore a11y-missing-attribute -->
+			<a tabindex="-1" class="dropdown-item text-sm menu-item-disabled">Settings</a>
+			<!-- svelte-ignore a11y-missing-attribute -->
+			<a tabindex="-1" class="dropdown-item text-sm">Logout</a>
+		</div>
+	</div>
+</section>
