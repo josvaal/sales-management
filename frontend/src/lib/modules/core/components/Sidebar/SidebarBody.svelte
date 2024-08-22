3fef05e189783ab018e28df9f46aff1cd7cf5ffe
josvaal <josval.personal@gmail.com>
2024-08-22 15:34:12 -0500

    feat: Sidebar modified for modulate

--- frontend/src/lib/modules/core/components/Sidebar/SidebarBody.svelte
+++ frontend/src/lib/modules/core/components/Sidebar/SidebarBody.svelte
@@ -1,46 +1,29 @@
+<script>
+	import Button from './Item/Button.svelte';
+</script>
+
 <section class="sidebar-content min-h-[20rem]">
 	<nav class="menu rounded-md">
 		<section class="menu-section px-4">
 			<span class="menu-title">Main menu</span>
 			<ul class="menu-items">
-				<li class="menu-item menu-active">
-					<span>Summary</span>
+				<li class="menu-item">
+					<Button items={[{ title: 'Summary', url: '#' }]} />
 				</li>
 
 				<li class="menu-item">
-					<span>Users</span>
+					<Button items={[{ title: 'Users', url: '#' }]} />
 				</li>
 				<li>
-					<input type="checkbox" id="menu-2" class="menu-toggle" />
-					<label class="menu-item justify-between" for="menu-2">
-						<div class="flex gap-2">
-							<span>Reports</span>
-						</div>
-						<span class="menu-icon">
-							<svg
-								xmlns="http://www.w3.org/2000/svg"
-								class="h-5 w-5"
-								viewBox="0 0 20 20"
-								fill="currentColor"
-							>
-								<path
-									fill-rule="evenodd"
-									d="M5.293 7.293a1 1 0 011.414 0L10 10.586l3.293-3.293a1 1 0 111.414 1.414l-4 4a1 1 0 01-1.414 0l-4-4a1 1 0 010-1.414z"
-									clip-rule="evenodd"
-								/>
-							</svg>
-						</span>
-					</label>
-
-					<div class="menu-item-collapse">
-						<!-- svelte-ignore a11y-label-has-associated-control -->
-						<div class="min-h-0">
-							<label class="menu-item ml-6">Daily</label>
-							<label class="menu-item ml-6">Monthly</label>
-							<label class="menu-item ml-6">Yearly</label>
-							<label class="menu-item ml-6">All</label>
-						</div>
-					</div>
+					<Button
+						items={[
+							{ title: 'Reports', url: '#' },
+							{ title: 'Daily', url: '#' },
+							{ title: 'Monthly', url: '#' },
+							{ title: 'Yearly', url: '#' },
+							{ title: 'Alll', url: '#' }
+						]}
+					/>
 				</li>
 			</ul>
 		</section>
